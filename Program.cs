3b1154acd1ac86ca3285e3ffaefa578e3d5e5689
Kenya Chan <kenya.chan@gmail.com>
2021-03-09 22:55:25 +1100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Office.Interop.Word;
+
+namespace MailMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
